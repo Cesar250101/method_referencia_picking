803f53471b3c76f363a20d6a8973a96ddfb6455d
Your Name <you@example.com>
2021-08-17 17:22:06 -0400

    Referencias

--- models/models.py
+++ models/models.py
@@ -12,14 +12,15 @@ class Pedidos(models.Model):
         pedidoinvoice_vals = super(Pedidos, self).action_confirm()
         picking_id=self.env['stock.picking'].search([('sale_id','=',self.id)]).id
         referencias=self.referencia_ids
-        for r in referencias:
-            vals={
-                    "origen": r.folio,
-                    "sii_referencia_TpoDocRef": r.sii_referencia_TpoDocRef.id,
-                    "motivo": r.motivo,
-                    "fecha_documento": r.fecha_documento,
-                    "stock_picking_id":picking_id,
-                }      
-        self.env['stock.picking.referencias'].create(vals)    
+        if referencias:
+            for r in referencias:
+                vals={
+                        "origen": r.folio,
+                        "sii_referencia_TpoDocRef": r.sii_referencia_TpoDocRef.id,
+                        "motivo": r.motivo,
+                        "fecha_documento": r.fecha_documento,
+                        "stock_picking_id":picking_id,
+                    }      
+            self.env['stock.picking.referencias'].create(vals)    
                         
 
